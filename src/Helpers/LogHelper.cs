6da860ef26b01892064ce87f0a1248776cad13e2
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2021-05-16 14:50:50 +0300

    Fix copyright notices (#22)
    * Fix copyright notices
    
    * Fix URLs
    
    Co-authored-by: Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>

--- src/Helpers/LogHelper.cs
+++ src/Helpers/LogHelper.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2018-2021 The SOSIEL Foundation. All rights reserved.
+﻿// Copyright (C) 2021 SOSIEL Inc. All rights reserved.
 // Use of this source code is governed by a license that can be found
 // in the LICENSE file located in the repository root directory.
 
